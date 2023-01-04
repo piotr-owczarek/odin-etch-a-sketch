e038ee5a7b13f96e1cb1b66c940599d40bcd2d54
Piotr Owczarek <piotrek.zwd@gmail.com>
2023-01-04 15:05:04 +0100

    Added coloring, sizing and clearing

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-let rows = 10;
-const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
+const colorPicker = document.getElementById('color-picker');
+const sizePicker = document.getElementById('size-picker');
+const clearButton = document.getElementById('clear-button');
+let color = colorPicker.value;
+let size = 32;
+let mouseDown = false;
+grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr);`;
 
-for (let i = 0; i < rows*rows; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
+function changeColor() {
+    if (!mouseDown) return;
+    this.style.cssText = `background: ${color};`;
 }
 
-container.style.cssText = `grid-template-columns: repeat(${rows}, 1fr);`
+function createGrid(){
+    grid.replaceChildren();
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseover', changeColor);
+        grid.appendChild(cell);
+    }
+}
+
+colorPicker.addEventListener('change', () => {
+    color = colorPicker.value;
+})
+
+sizePicker.addEventListener('change', () => {
+    size = sizePicker.value;
+    createGrid();
+    grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr);`;
+})
+
+document.body.addEventListener('mousedown', () => mouseDown = true);
+document.body.addEventListener('mouseup', () => mouseDown = false);
+clearButton.addEventListener('click', createGrid);
+createGrid();
